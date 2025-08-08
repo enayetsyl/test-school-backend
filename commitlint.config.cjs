b4114da6ad4fd254228625f5023924df1ec72951
enayetsyl <enayetflweb@gmail.com>
2025-08-08 17:04:35 +0600

    chore: wire up husky and commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,30 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'build',
+        'ci',
+        'revert',
+      ],
+    ],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'scope-case': [2, 'always', 'kebab-case'],
+    // loosen header length if you want
+    'header-max-length': [1, 'always', 100],
+  },
+};
